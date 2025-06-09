6adfd9f59a4d698c6dd5de00a09f88b9738c8767
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-06-09 11:03:30 +0100

    feat: Configured PostgreSQL via Docker in Multipass and applied users table migration

---
+++ backend/migrations/20250609094139_create_users_table.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- Add down migration script here
+DROP TABLE IF EXISTS users;
